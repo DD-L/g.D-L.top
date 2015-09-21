b4cdfa1023a013b113d518c249b617f0947c033f
deel <deel@d-l.top>
2015-09-21 19:07:52 +0800

    mv nginxfiles to $OPENSHIFT_DATA_DIR/nginx

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,16 +13,19 @@ source $OPENSHIFT_CARTRIDGE_SDK_BASH
 BUILT_DIR=$OPENSHIFT_TMP_DIR/nginx_built
 test -d $BUILT_DIR || mkdir -p $BUILT_DIR
 
+NGINX_DIR=$OPENSHIFT_DATA_DIR/nginx
+test -d $NGINX_DIR || mkdir -p $NGINX_DIR
+
 cd $BUILT_DIR
 
 # pcre, zlib, openssl, sha1
 wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.37.tar.bz2
 wget http://zlib.net/zlib-1.2.8.tar.gz
-wget ftp://ftp.openssl.org/source/openssl-1.0.2.tar.gz
+#wget ftp://ftp.openssl.org/source/openssl-1.0.2.tar.gz
 #wget http://www.tamale.net/sha1/sha1-0.2.tar.gz 
 tar jxvf pcre-8.37.tar.bz2
 tar xvf zlib-1.2.8.tar.gz
-tar xvf openssl-1.0.2.tar.gz
+#tar xvf openssl-1.0.2.tar.gz
 #tar xvf sha1-0.2.tar.gz
 
 # nginx
@@ -31,25 +34,25 @@ tar zxvf nginx-1.9.4.tar.gz
 
 # configure
 cd $BUILT_DIR/nginx-1.9.4
-./configure --prefix=$OPENSHIFT_DATA_DIR --with-pcre=$OPENSHIFT_TMP_DIR/nginx_built/pcre-8.37 --with-zlib=$OPENSHIFT_TMP_DIR/nginx_built/zlib-1.2.8  --with-ipv6 --with-http_stub_status_module --with-http_gzip_static_module --with-http_sub_module --with-http_ssl_module
+./configure --prefix=$NGINX_DIR --with-pcre=$OPENSHIFT_TMP_DIR/nginx_built/pcre-8.37 --with-zlib=$OPENSHIFT_TMP_DIR/nginx_built/zlib-1.2.8  --with-ipv6 --with-http_stub_status_module --with-http_gzip_static_module --with-http_sub_module --with-http_ssl_module
 
 #--with-openssl=/tmp/nginx_built/openssl-1.0.2
 #--with-sha1=/tmp/nginx_built/sha1-0.2
 
 if [ $? -ne 0 ]; then
-	echo -e "\nbuild nginx: An error occurred. './configure...'\n"
+	echo -e "\nAn error occurred. build nginx: './configure...'\n"
 	exit 1
 fi
 
 make
 if [ $? -ne 0 ]; then
-	echo -e "\nbuild nginx: An error occurred. 'make...'\n"
+	echo -e "\nAn error occurred. build nginx: 'make...'\n"
 	exit 1
 fi
 
 make install
 if [ $? -ne 0 ]; then
-	echo -e "\nbuild nginx: An error occurred. 'make install...'\n"
+	echo -e "\nAn error occurred. install nginx: 'make install...'\n"
 	exit 1
 fi
 
@@ -58,7 +61,7 @@ fi
 rm -rf $BUILT_DIR
 
 # backup - nginx.conf
-cp -a $OPENSHIFT_DATA_DIR/conf/nginx.conf $OPENSHIFT_DATA_DIR/conf/nginx.conf.bak
+cp -a $NGINX_DIR/conf/nginx.conf $NGINX_DIR/conf/nginx.conf.bak
 
 echo -e "\nDeployment has been completed...\n"
 #exit 0
